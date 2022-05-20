3636ca7621f402d1870c4dcad73e3ad720d69ecf
Michael Frolov <moyshedev@gmail.com>
2022-05-20 17:44:20 +0300

    Made the numerator and denominator attributes private in the class. Implemented access to attributes through properties.

--- w3/w3.1.py
+++ w3/w3.1.py
@@ -4,3 +4,11 @@ class Fraction:
     def __init__(self, num: int, den: int):
         self.__numerator = num
         self.__denominator = den
+
+    @property
+    def get_num(self):
+        return self.__numerator
+
+    @property
+    def get_den(self):
+        return self.__denominator
