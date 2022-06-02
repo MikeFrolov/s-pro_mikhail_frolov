adbb3c26e4f0ab5e9238b32963ea04e83763ef51
Michael Frolov <moyshedev@gmail.com>
2022-06-02 12:28:30 +0300

    Implemented the distance method that takes an instance of the Point2D class and calculates the distance between 2 points on the plane

--- w3/w3.2.py
+++ w3/w3.2.py
@@ -1,5 +1,11 @@
+from math import hypot
+
+
 class Point2D:
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def distance(self, p2):
+        return hypot(p2.x - self.x, p2.y - self.y)
