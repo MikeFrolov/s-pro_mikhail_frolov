228fbe22887e4dfd24f9af67a8c92263e26bb918
Michael Frolov <moyshedev@gmail.com>
2022-06-02 18:59:45 +0300

    The class Point3D is created, inherited from the class Point2D. The point is given by 3 parameters - coordinates x, y, z. The constructor is overridden with super()

--- w3/w3.2.py
+++ w3/w3.2.py
@@ -1,17 +1,22 @@
-from math import hypot
+from math import sqrt
 
 
 class Point2D:
     __count = 0
 
-    def __init__(self, x, y):
+    def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
         Point2D.__count += 1
 
     def distance(self, p2):
-        return hypot(p2.x - self.x, p2.y - self.y)
+        return sqrt(((self.x - p2.x) ** 2) + ((self.y - p2.y) ** 2))
 
-    @property
     def get_count(self):
         return self.__count
+
+
+class Point3D(Point2D):
+    def __init__(self, x=0, y=0, z=0):
+        super().__init__(x, y)
+        self.z = z
