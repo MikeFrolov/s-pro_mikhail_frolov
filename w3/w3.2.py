b79071a8a52fe669d1608d3189e817f31c1e3b43
Michael Frolov <moyshedev@gmail.com>
2022-06-02 12:59:43 +0300

    Created a protected class attribute - the counter of created instances of the class, reading the number of instances is implemented through the getter method

--- w3/w3.2.py
+++ w3/w3.2.py
@@ -2,10 +2,16 @@ from math import hypot
 
 
 class Point2D:
+    __count = 0
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        Point2D.__count += 1
 
     def distance(self, p2):
         return hypot(p2.x - self.x, p2.y - self.y)
+
+    @property
+    def get_count(self):
+        return self.__count
