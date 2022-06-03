471606b3e0fe5979d384e1236049e70cafad6e1f
Michael Frolov <moyshedev@gmail.com>
2022-06-03 22:26:24 +0300

    Defined a generator function fib_list(n)

--- w4/w4.3.py
+++ w4/w4.3.py
@@ -3,3 +3,13 @@ def fib_generator(n):
     for i in range(n):
         yield a
         a, b = b, a + b
+
+
+def fib_list(n):
+    # return [i for i in fib_generator(n)]
+    a, b = 0, 1
+    sequence = []
+    for i in range(n):
+        sequence.append(a)
+        a, b = b, a + b
+    return sequence
