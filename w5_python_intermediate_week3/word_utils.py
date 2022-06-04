51d5a5fe9251282302d70daaf13f9e17cc8fe214
Michael Frolov <moyshedev@gmail.com>
2022-06-04 12:00:29 +0300

    Implemented a function that returns the longest word from a sentence in the 'word_utils.py' module

--- w5_python_intermediate_week3/word_utils.py
+++ w5_python_intermediate_week3/word_utils.py
@@ -8,3 +8,7 @@ def punctuation_cleaning(line: str) -> str:
 def word_list(line: str) -> list:
     clear_line = punctuation_cleaning(line)  # used punctuation_cleaning function
     return [word for word in clear_line.split(" ") if word != '']
+
+
+def longest_word(line: str) -> str:
+    return max(word_list(line), key=len)  # used word_list function
